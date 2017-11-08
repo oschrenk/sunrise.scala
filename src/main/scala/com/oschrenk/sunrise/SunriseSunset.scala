a538ac6fb9e532ffeec319d2e3ed58c994eb1995
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-11-08 22:39:42 +0100

    Hoist more constants to class level

--- src/main/scala/com/oschrenk/sunrise/SunriseSunset.scala
+++ src/main/scala/com/oschrenk/sunrise/SunriseSunset.scala
@@ -20,6 +20,26 @@ object SunriseSunset {
     .appendValue(JulianFields.JULIAN_DAY)
     .toFormatter().withZone(UTC)
 
+  // mean anomaly at a fixed moment in degrees
+  private val m0 = BigDecimal("357.5291")
+  // the rate of change of the mean anomaly in degrees per day
+  private val m1 = BigDecimal("0.98560028")
+
+  // equation of center coefficients
+  private val C1 = BigDecimal("1.9148")
+  private val C2 = BigDecimal("0.0200")
+  private val C3 = BigDecimal("0.0003")
+
+  // simplified equation of time coefficients in fractional day minutes.
+  private val D1 = BigDecimal("0.0053")
+  private val D2 = BigDecimal("-0.0069")
+
+  private val altitudeSolarDiscInDegrees = BigDecimal("-0.83")
+  // we assume sea level for observer
+  private val elevationInMeter = 0
+  private val observerCorrection = BigDecimal("-2.076") * Math.sqrt(elevationInMeter) / 60
+  private val sinOfAltitudeSolarDisc = sin(toRadians((altitudeSolarDiscInDegrees + observerCorrection).toDouble))
+
   private def toZonedDate(julianDay: BigDecimal, zoneId: ZoneId): ZonedDateTime = {
     val days = Math.floor(julianDay.toDouble).toInt
     val fraction = julianDay - days
@@ -34,25 +54,24 @@ object SunriseSunset {
   def of(latitude: Double, longitude: Double, date: LocalDate, zoneId: ZoneId = ZoneId.systemDefault()): (ZonedDateTime, ZonedDateTime) = {
     val nStar = date.getLong(JulianFields.JULIAN_DAY) - J2000 + Drift - longitude / 360
 
-    val m0 = BigDecimal("357.5291") // in degrees
-    val m1 = BigDecimal("0.98560028") // in degrees per day
     val meanAnomaly = (m0 + m1 * nStar) % 360
     val meanAnomalyRadians = toRadians(meanAnomaly.toDouble)
 
     val equationOfCenter =
-      BigDecimal("1.9148") * sin(meanAnomalyRadians) +
-        BigDecimal("0.0200") * sin(2 * meanAnomalyRadians) +
-        BigDecimal("0.0003") * sin(3 * meanAnomalyRadians)
+      C1 * sin(meanAnomalyRadians) +
+        C2 * sin(2 * meanAnomalyRadians) +
+        C3 * sin(3 * meanAnomalyRadians)
     val eclipticalLongitude = (meanAnomaly + equationOfCenter + PerhilionEarth + 180) % 360
-    //  The coefficients are fractional day minutes.
+
     val simplifiedEquationOfTime =
-      BigDecimal("0.0053") * sin(meanAnomalyRadians.toDouble) +
-        BigDecimal("-0.0069") * sin(2 * toRadians(eclipticalLongitude.toDouble))
+      D1 * sin(meanAnomalyRadians.toDouble) +
+        D2 * sin(2 * toRadians(eclipticalLongitude.toDouble))
+
     // 0.5 is midnight
     val jTransit = J2000 + 0.5 + nStar + simplifiedEquationOfTime
 
     val sunDeclination = sin(toRadians(eclipticalLongitude.toDouble)) * sin(toRadians(EarthObliquity.toDouble))
-    val observerCorrection = sin(toRadians(BigDecimal("-0.83").toDouble))
+    val observerCorrection = sinOfAltitudeSolarDisc
     val hourAngle = Math.acos(
       (observerCorrection - sin(latitude.toRadians) * sin(sunDeclination)) /
         (cos(latitude.toRadians) * cos(sunDeclination))
